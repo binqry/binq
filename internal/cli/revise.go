4a32df45f221e0ac5a629c0cb272a2756397766b
progrhyme <progrhyme@gmail.com>
2020-07-03 12:34:24 +0900

    [enhance][cli/revise] Show colorized diff & update JSON file

--- internal/cli/revise.go
+++ internal/cli/revise.go
@@ -1,13 +1,19 @@
 package cli
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 	"text/template"
 
+	"github.com/gookit/color"
+	"github.com/pmezard/go-difflib/difflib"
 	"github.com/progrhyme/binq/internal/logs"
 	"github.com/progrhyme/binq/schema/item"
 	"github.com/spf13/pflag"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type reviseCmd struct {
@@ -17,7 +23,7 @@ type reviseCmd struct {
 
 type reviseOpts struct {
 	urlFormat, replacements, extensions, checksums *string
-	delete, latest, noLatest                       *bool
+	delete, latest, noLatest, yes                  *bool
 	*commonOpts
 }
 
@@ -31,6 +37,7 @@ func newReviseCmd(common *commonCmd) (self *reviseCmd) {
 		replacements: fs.StringP("replace", "r", "", "# JSON parameter for \"replacements\""),
 		extensions:   fs.StringP("ext", "e", "", "# JSON parameter for \"extensions\""),
 		checksums:    fs.StringP("sum", "s", "", "# JSON parameter for \"checksums\""),
+		yes:          fs.BoolP("yes", "y", false, "# Update JSON file without confirmation"),
 		delete:       fs.Bool("delete", false, "# Delete version"),
 		latest:       fs.Bool("latest", false, "# Add or Update as Latest Version"),
 		noLatest:     fs.Bool("no-latest", false, "# Add or Update as Not Latest Version"),
@@ -50,10 +57,10 @@ Usage:
   # Add or Update Version
   <<.prog>> <<.name>> path/to/item.json VERSION \
     [-s|--sum CHECKSUMS] [-u|--url URL_FORMAT] [-r|--replace REPLACEMENTS] [-e|--ext EXTENSIONS] \
-    [--latest] [--no-latest]
+    [--latest] [--no-latest] [-y|--yes]
 
   # Delete Version
-  <<.prog>> <<.name>> path/to/item.json VERSION [--delete]
+  <<.prog>> <<.name>> path/to/item.json VERSION [--delete] [-y|--yes]
 
 Examples:
   # Add v0.1.1 if not exist
@@ -97,13 +104,13 @@ func (cmd *reviseCmd) run(args []string) (exit int) {
 	}
 
 	file, version := args[0], args[1]
-	b, err := ioutil.ReadFile(file)
+	orig, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Fprintf(cmd.errs, "Error! Can't read file: %s. %v\n", file, err)
 		return exitNG
 	}
 
-	obj, err := item.DecodeItemJSON(b)
+	obj, err := item.DecodeItemJSON(orig)
 	if err != nil {
 		fmt.Fprintf(cmd.errs, "Error! Failed to decode Item JSON. %v\n", err)
 		return exitNG
@@ -116,7 +123,7 @@ func (cmd *reviseCmd) run(args []string) (exit int) {
 			return exitNG
 		}
 		logs.Debugf("Version %s deleted. After Item: %s", version, obj)
-		return cmd.writeRevisedItem(obj)
+		return cmd.writeRevisedItem(obj, file, orig)
 	}
 
 	var replacements, extensions map[string]string
@@ -145,15 +152,82 @@ func (cmd *reviseCmd) run(args []string) (exit int) {
 	obj.AddOrUpdateRevision(rev, mode)
 	logs.Debugf("Version %s updated. After Item: %s", version, obj)
 
-	return cmd.writeRevisedItem(obj)
+	return cmd.writeRevisedItem(obj, file, orig)
 }
 
-func (cmd *reviseCmd) writeRevisedItem(obj *item.Item) (exit int) {
-	b, err := obj.Print(true)
+func (cmd *reviseCmd) writeRevisedItem(obj *item.Item, src string, orig []byte) (exit int) {
+	revised, err := obj.Print(true)
 	if err != nil {
 		fmt.Fprintf(cmd.errs, "Error! Failed to print Item JSON. %v\n", err)
 		return exitNG
 	}
-	fmt.Fprintf(cmd.outs, "%s\n", b)
+
+	diff := difflib.UnifiedDiff{
+		A:        difflib.SplitLines(strings.TrimRight(string(orig), "\r\n")),
+		B:        difflib.SplitLines(string(revised)),
+		FromFile: src,
+		ToFile:   "Revised",
+		Context:  3,
+	}
+	text, err := difflib.GetUnifiedDiffString(diff)
+	if err != nil {
+		fmt.Fprintf(cmd.errs, "Error! Can't get diff. %v\n", err)
+		return exitNG
+	}
+	if text == "" {
+		fmt.Fprintln(cmd.errs, "No change")
+	} else {
+		if !*cmd.option.yes {
+			if terminal.IsTerminal(1) {
+				fmt.Fprintln(cmd.outs, colorizeDiff(text))
+			} else {
+				fmt.Fprintln(cmd.outs, text)
+			}
+		}
+		if terminal.IsTerminal(0) && !*cmd.option.yes {
+			fmt.Fprintf(cmd.errs, "Overwrite %s? (Y/n) ", src)
+			stdin := bufio.NewScanner(os.Stdin)
+			stdin.Scan()
+			ans := stdin.Text()
+			if strings.HasPrefix(ans, "n") || strings.HasPrefix(ans, "N") {
+				fmt.Fprintln(cmd.errs, "Canceled")
+				return exitOK
+			}
+		}
+
+		file, err := os.OpenFile(src, os.O_WRONLY|os.O_TRUNC, 0664)
+		if err != nil {
+			fmt.Fprintf(cmd.errs, "Error! Can't open file: %s\n", src)
+			return exitNG
+		}
+		defer file.Close()
+		if _, err = file.Write(revised); err != nil {
+			fmt.Fprintf(cmd.errs, "Error! Can't write file: %s\n", src)
+			return exitNG
+		}
+		fmt.Fprintf(cmd.outs, "Updated %s\n", src)
+	}
+
 	return exitOK
+}
+
+func colorizeDiff(diff string) (colored string) {
+	lines := strings.Split(diff, "\n")
+	for i, s := range lines {
+		switch {
+		case strings.HasPrefix(s, "---"):
+			lines[i] = color.Danger.Render(s)
+		case strings.HasPrefix(s, "+++"):
+			lines[i] = color.Success.Render(s)
+		case strings.HasPrefix(s, "-"):
+			lines[i] = color.Red.Render(s)
+		case strings.HasPrefix(s, "+"):
+			lines[i] = color.Green.Render(s)
+		case strings.HasPrefix(s, "@@"):
+			lines[i] = color.Note.Render(s)
+		default:
+			// Nothing to do
+		}
+	}
+	return strings.Join(lines, "\n")
 }
