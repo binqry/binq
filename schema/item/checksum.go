4fb50378c8ba8f41743ed17564c193a75b1ccce2
progrhyme <progrhyme@gmail.com>
2020-07-04 21:17:50 +0900

    [refactor] Rename schema/item.ItemChecksums -> ItemChecksum
    And add comment for CRC spec

--- schema/item/checksum.go
+++ schema/item/checksum.go
@@ -6,13 +6,14 @@ import (
 	"github.com/progrhyme/binq/internal/logs"
 )
 
-type ItemChecksums struct {
+type ItemChecksum struct {
 	File   string `json:"file"`
 	SHA256 string `json:"sha256,omitempty"`
-	CRC    string `json:"crc,omitempty"`
+	// CRC-32 IEEE Std.
+	CRC string `json:"crc,omitempty"`
 }
 
-func NewItemChecksums(arg string) (sums []ItemChecksums) {
+func NewItemChecksums(arg string) (sums []ItemChecksum) {
 	if arg == "" {
 		return nil
 	}
@@ -21,13 +22,13 @@ func NewItemChecksums(arg string) (sums []ItemChecksums) {
 		params := strings.Split(entry, ":")
 		switch len(params) {
 		case 2:
-			sums = append(sums, ItemChecksums{File: params[0], SHA256: params[1]})
+			sums = append(sums, ItemChecksum{File: params[0], SHA256: params[1]})
 		case 3:
 			switch params[2] {
 			case "sha256", "SHA256", "SHA-256":
-				sums = append(sums, ItemChecksums{File: params[0], SHA256: params[1]})
+				sums = append(sums, ItemChecksum{File: params[0], SHA256: params[1]})
 			case "crc", "CRC":
-				sums = append(sums, ItemChecksums{File: params[0], CRC: params[1]})
+				sums = append(sums, ItemChecksum{File: params[0], CRC: params[1]})
 			default:
 				logs.Warnf("Unsupported algorithm: %s. Param: %s", params[2], entry)
 			}
